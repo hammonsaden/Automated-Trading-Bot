6bad6caa99ec7a2c303564e2e431db76235e32ae
hammonsaden <110563302+hammonsaden@users.noreply.github.com>
2024-11-06 20:06:31 -0500

    1st day work
    got a good amount done. hoping to be able to trade tomorrow.

--- READ-ME.md
+++ READ-ME.md
@@ -1,4 +1,4 @@
-# Automater-Trading-Bot
+# Automated-Trading-Bot
 
 
 This is my second program I've built for my resume, its a "Automated" Trading Bot built using Python, SQL, and a API called "Finnhub" for all stock data.
@@ -11,3 +11,59 @@ You can view the amount of money your investing portfolio is currently worth (Fa
 You can view data for any stock using the search bar at the top of the screen (current stock price, high price of the day, low price of the day, previous close price.)
 You can add stocks to your watch list for quick searching.
 You can view previous trades made by the bot automatically.
+
+
+ ~ 11/6/2024 ~
+I had to do some extra research on some of the functions of this program, because I wasn't exactly sure the route I wanted to go with it. As far as how I will program the trading and 
+whether or not to utilize a AI neural network or not. I figured out I will hard code the functions in this and let it make decisions based off of that. I do want to limit the amount of trades per hour to 6,
+so it wont just trade every time there is a little change in the program. I also had trouble figuring out how I wanted to display graphs, due to the fact that I found out that I couldn't use finnhubs graphs to put directly into my program, so I decided I will utilize a python module called Mathplotlib (I have never utilized this before, so I am excited) to form my graphs and put them into my customtkinter GUI.
+
+I am planning to have a order in which I want to do things to hopefully make it as smooth as possible in creating this program.
+
+I would like to get as much of the front end done first, but I know that my login page will probably be the first thing that needs to be completed in whole.
+
+I ended up doing the login page with Custom Tkinter and a hasher module named bcrypt for encrypting the passwords in my users sql file, happy with how it turned out. I added 2 different accounts with 1 having user and password "test1" and the other having the user and password "test2". Both of these accounts are going to mirror eachother and have no data difference, but it will show that I can make a functioning login page that can encrypt.
+
+Moving onto the main_GUI of the program. I have a couple key points of data I want it to display when the user first opens up the program. first, I want it to display the current users name in the top left corner, and I want it to display the current value of the portfolio next to that.
+
+As far as how I want my data structure is going to be, here is an idea of how I want it organized:
+
+SQLite3
+
+~ users.db ~ 
+    - users table
+        - username (TEXT)
+        - password_hash (TEXT)
+
+~ portfolio.db ~
+    - portfolio table
+        - datetime (TEXT)
+        - value (REAL)
+    - trades table
+        - datetime (TEXT)
+        - ticker (TEXT)
+        - amount (REAL)
+        - value (REAL)
+    - portfolio_stocks table
+        - ticker (TEXT)
+        - amount (REAL)
+    - watchlist table
+        - ticker (TEXT)
+        - last_price (REAL)
+        - amount_owned (REAL)
+        - approx_value (REAL)
+
+This is just how I think I want my data to be organized, I might end up changing it down the line. My users.db will stay the same, it functions as needed with my program already and will only be used for the logging in portion of the program. As far as my portfolio.db code goes. I will work on that now as well as getting my basic GUI elements organized before I hook the program upto the finnhubb API.
+
+6:07pm
+I was able to get the portfolio page setup like I want it. I made a scrollable frame with a graph displaying the portfolio value. I also just implemented my finnhub API and made my stock search bar operational with any stock or ETF on the US Stock Market, which was pretty cool to be able to utilize all of that data.
+I am implementing a watch list for the user currently, that they can add and take stocks from as they please. ANY stocks on the users watchlist will be watched and traded by the bot. At this point, I have hit most points in the original idea of my program, but I know most of the work is going to go into storing data per stock and having the bot analyze it and make trades based off of that data. I am going to utilize my portfolio_stocks table to track the price of the stocks on the user's watchlist, to form a price history to be able to make more informed decisions.
+
+6:44pm
+I just finished polishing off the stock search and making sure the grid lined up perfectly, and I added a button for when you search a stock, it will ask you if you want to add it to the watch list. I am going to take part of the bottom section of the stock search and turn it into the area where you can see what is on your watch list and edit your watch list.
+
+7:02pm
+Finished the watchlist section, and now when you press the watch button under the stock when it pulls up, it was automatically append the stock ticker to your watchlist. At this moment, I am going to also recogfigure my SQL table settings to add another table under portfolio.db that is the watchlist table, which will hold the data "ticker, last_price, amount_owned, approx_value".
+
+7:59pm
+I am done for tonight, should be able to configure the rest of the bot tomorrow so that it can hopefully do it's first day of trading tomorrow on the market with the $1,000.00 I have given it. I just ending up getting the watchlist done completely how I wanted it. I had to redo the backend on it, because I did it the simple way earlier that I knew I was going to have to override. I had to go back and hook it up to the watchlist SQL table and have it read the values from there and put it onto a table on the screen, rather than basically hard coding in the values through a jumbled mess of references to different spots. This way should make it much more efficient to handle the watchlist later on. I still haven't added the feature to delete stuff from the watchlist yet, but I will do so tomorrow. I also want to make a function to redraw the plot each time the portfolio value changes (I am thinking I want it to update every 5 minutes or so, so it can update and base the value off of the "somewhat" current market prices (meaning upto 5 minutes old.)). The bot will have to trade based off of a quicker time than that I am thinking. The limit for the bot will be 1 trade every 10 minutes, but I want it to have access to more current prices than that so it can make it's decision.
--- main.py
+++ main.py
@@ -1,3 +1,256 @@
 import sqlite3
 import requests
 import customtkinter as ctk
+import csv
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import bcrypt
+from datetime import datetime
+import finnhub
+
+
+class TraderBot:
+    def __init__(self):
+        pass
+
+class TraderApp:
+    def __init__(self):
+        self.app = ctk.CTk()
+        self.loginconn = sqlite3.connect('users.db')
+        self.portfolioconn = sqlite3.connect('portfolio.db')
+        self.loginFrame = None
+        self.bot = TraderBot()
+        self.watchlist = []
+        # Finnhub
+        self.API_KEY = 'co7coipr01qgik2h3bt0co7coipr01qgik2h3btg'
+        self.finnhub = finnhub.Client(api_key=self.API_KEY)
+    def window(self):
+        ctk.set_appearance_mode("light")
+        self.app.title("Automated Trading Bot")
+        self.app.geometry("1600x700")
+        self.Login_GUI()
+
+    def Login_GUI(self):
+        self.loginFrame = ctk.CTkFrame(self.app)
+        self.loginFrame.pack(fill="both", expand=True, padx=20, pady=20)
+
+        # Spacers to Center Content
+        self.loginFrame.grid_rowconfigure(0, weight=1)  
+        self.loginFrame.grid_rowconfigure(1, weight=1)  
+        self.loginFrame.grid_rowconfigure(4, weight=1)  
+
+        self.loginFrame.grid_columnconfigure(0, weight=1)  
+        self.loginFrame.grid_columnconfigure(1, weight=1)  
+        self.loginFrame.grid_columnconfigure(2, weight=1)  
+
+        # Add login header and entry widgets
+        loginHeader = ctk.CTkLabel(self.loginFrame, text='LOGIN')
+        loginHeader.grid(row=0, column=1, pady=(0, 10))
+
+        # Username
+        usrtext = ctk.CTkLabel(self.loginFrame, text="Username: ")
+        usrtext.grid(row=1, column=0, sticky="e", padx=10)
+        self.userinput = ctk.CTkEntry(self.loginFrame, placeholder_text='Username')
+        self.userinput.grid(row=1, column=1, sticky="we", padx=10)  
+
+        # Password
+        pswdtext = ctk.CTkLabel(self.loginFrame, text="Password: ")
+        pswdtext.grid(row=2, column=0, sticky="e", padx=10)
+        self.pswdinput = ctk.CTkEntry(self.loginFrame, placeholder_text="Password")
+        self.pswdinput.grid(row=2, column=1, sticky="we", padx=10, pady=20)
+
+        # Enter Button
+        etrButton = ctk.CTkButton(self.loginFrame, text="Enter", command=self.Login_Info_Handler)
+        etrButton.grid(row=3, column=1, sticky="we", padx=10)
+        
+    def Login_Info_Handler(self):
+        cur = self.loginconn.cursor()
+        
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL
+            )
+    ''')
+        
+        self.entered_user = self.userinput.get()
+        entered_pswd = self.pswdinput.get()
+
+        # Verify Password
+        
+        cur.execute("SELECT password_hash FROM users WHERE username = ?", (self.entered_user,))
+        r = cur.fetchone()
+        if r:
+            stored_password_hash = r[0]
+            if bcrypt.checkpw(entered_pswd.encode(), stored_password_hash):
+                print("User Logged in!")
+                # Transfer us to the main screen in the program
+                self.main_GUI()
+        else:
+            print("User not found!")
+    
+    def main_GUI(self):
+        if self.loginFrame is not None:
+            self.loginFrame.destroy()
+        
+        mainFrame = ctk.CTkFrame(self.app)
+        mainFrame.pack(fill="both", expand=True, padx=20, pady=20)
+
+        # Display user label
+        activity_text = ctk.CTkLabel(mainFrame, text="User: " + self.entered_user, font=("Helvetica", 18))
+        activity_text.grid(row=0, column=0, sticky="w", padx=10)
+
+        # Fetch last known portfolio value
+        cur = self.portfolioconn.cursor()
+        cur.execute("SELECT value FROM portfolio ORDER BY datetime DESC LIMIT 1")
+        last_val = cur.fetchone()
+
+        if last_val:
+            portfolio_val = last_val[0]
+        else:
+            portfolio_val = 1000  # Default value if no data
+
+        # Display portfolio value label
+        value_text = ctk.CTkLabel(mainFrame, text="Portfolio Value $ " + str(portfolio_val), font=("Helvetica", 18))
+        value_text.grid(row=0, column=1, sticky="e", padx=10)
+
+        # Portfolio Frame
+        portfolio_frame = ctk.CTkScrollableFrame(mainFrame, width=760, height=625)
+        portfolio_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")  
+        portfolioheader = ctk.CTkLabel(portfolio_frame, text="PORTFOLIO", font=("Helvetica", 18))
+        portfolioheader.grid(row=0, column=0)
+
+        cur.execute("SELECT datetime, value FROM portfolio ORDER BY datetime")
+        rows = cur.fetchall()
+
+        x = [row[0] for row in rows]
+        y = [row[1] for row in rows]
+
+        # Making the Portfolio Graph
+        fig, ax = plt.subplots(figsize=(7,3.2))
+        if len(x) == len(y) and len(x) > 0:
+            ax.plot(x, y, marker='o', color='green')
+            ax.set_title("Portfolio Value")
+            ax.set_xlabel("Value Timestamp")
+            ax.set_ylabel("Value in Dollars")
+            ax.legend()
+
+        # Display Value Above Point
+        for i in range(len(x)):
+            ax.annotate(f"${y[i]:,.2f}",  
+                    (x[i], y[i]),     
+                    textcoords="offset points",  
+                    xytext=(0, 5),    
+                    ha='center')
+            
+        port_canvas = FigureCanvasTkAgg(fig, master=portfolio_frame)
+        port_canvas.draw()
+        port_canvas.get_tk_widget().grid(row=1, column=0)
+
+        # Stock Frame
+        self.stock_frame = ctk.CTkScrollableFrame(mainFrame, width=460, height=625)
+        self.stock_frame.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")  
+        stockheader = ctk.CTkLabel(self.stock_frame, text="STOCK SEARCH", font=("Helvetica", 18))
+        stockheader.grid(row=0, column=0, columnspan=2)
+        
+        # Stock Input
+        self.stock_input = ctk.CTkEntry(self.stock_frame, placeholder_text="Enter Stock Ticker Here!", width=420)
+        self.stock_input.grid(row=1, column=0, sticky='ew', padx=(0, 10))
+        stock_button = ctk.CTkButton(self.stock_frame, text="Search Stock!", command=self.stock_search)
+        stock_button.grid(row=1, column=1, sticky="ew")
+
+        # Search Frame Inside of Stock Frame
+        self.search_frame = ctk.CTkFrame(self.stock_frame, width=475, height=223)
+        self.search_frame.grid(row=2, column=0, columnspan=2, sticky='nsew', pady=(10, 0))
+
+
+        # Watch List Header + Frame inside of Stock Frame
+        watchlist_header = ctk.CTkLabel(self.stock_frame, text="WATCHLIST", font=("Helvetica", 18))
+        watchlist_header.grid(row=3, column=0, columnspan=2)
+
+        self.watch_frame = ctk.CTkScrollableFrame(self.stock_frame, width=475, height=250)
+        self.watch_frame.grid(row=4, column=0, columnspan=2, sticky="nsew", pady=(10, 0))
+
+        # Configure grid for the main frame
+        mainFrame.grid_columnconfigure(0, weight=1)  
+        mainFrame.grid_columnconfigure(1, weight=1)  
+        mainFrame.grid_rowconfigure(0, weight=0)   
+        mainFrame.grid_rowconfigure(1, weight=1)    
+
+    def stock_search(self):
+        # Clear Frame for new results
+        for w in self.search_frame.winfo_children():
+            w.destroy()
+        search_tick = self.stock_input.get()
+
+        data = self.finnhub.quote(str(search_tick))
+        self.data_list = list(data.values())
+        data_tags = ["Current Price","Change","Percent Change","High Price","Low Price","Open Price","Previous Close Price"]
+        i = 0
+        for i in range(len(data_tags)):
+            info_label = ctk.CTkLabel(self.search_frame, text=data_tags[i] + " : " + str(self.data_list[i]), font=("Helvetica", 12))
+            info_label.grid(row=2+i, column=0, sticky='w')
+            buffer_label = ctk.CTkLabel(self.search_frame, text="")
+            buffer_label.grid(row=2+i, column=1)
+            i+=1
+
+        watchlist_button = ctk.CTkButton(self.search_frame, text="Add to Watchlist", command=self.add_watchlist)
+        watchlist_button.grid(row=10, column=0, sticky='w')
+
+    def add_watchlist(self):
+        ticker = self.stock_input.get().upper()
+        cur = self.portfolioconn.cursor()
+        cur.execute("SELECT amount FROM portfolio_stocks WHERE ticker = ?", (ticker,))
+        amount_owned = cur.fetchone()
+
+        if amount_owned:
+            approx_val = amount_owned * float(self.data_list[0])
+            cur.execute("INSERT INTO watchlist (ticker, last_price, amount_owned, approx_value) VALUES (?, ?, ?, ?)", (self.stock_input.get().upper(), self.data_list[0], amount_owned, approx_val))
+        else:
+            cur.execute("INSERT INTO watchlist (ticker, last_price, amount_owned, approx_value) VALUES (?, ?, ?, ?)", (self.stock_input.get().upper(), self.data_list[0], 0, 0))
+        
+        for w in self.watch_frame.winfo_children():
+            w.destroy()
+
+        self.ticker_header = ctk.CTkLabel(self.watch_frame, text='Ticker', font=("Helvetica", 12))
+        self.ticker_header.grid(row=0, column=0, sticky='w', padx=30)
+        self.lp_header = ctk.CTkLabel(self.watch_frame, text='Last Price', font=("Helvetica", 12))
+        self.lp_header.grid(row=0, column=1, sticky='w', padx=30)
+        self.owned_header = ctk.CTkLabel(self.watch_frame, text='Amount Owned', font=("Helvetica", 12))
+        self.owned_header.grid(row=0, column=2, sticky='w', padx=30)
+        self.value_header = ctk.CTkLabel(self.watch_frame, text='Approximate Value ($)', font=("Helvetica", 12))
+        self.value_header.grid(row=0, column=3, sticky='w', padx=30)
+
+        cur.execute("SELECT ticker, last_price, amount_owned, approx_value FROM watchlist")
+        self.watchlist = cur.fetchall()
+
+        for i, item in enumerate(self.watchlist):
+            ticker, last_price, amount_owned, approx_value = item  # Unpack the values
+            ticker_label = ctk.CTkLabel(self.watch_frame, text=f"{ticker}", font=("Helvetica", 12))
+            ticker_label.grid(row=i+1, column=0)
+    
+            last_price_label = ctk.CTkLabel(self.watch_frame, text=f"${last_price}", font=("Helvetica", 12))
+            last_price_label.grid(row=i+1, column=1)
+    
+            amount_owned_label = ctk.CTkLabel(self.watch_frame, text=f"{amount_owned}", font=("Helvetica", 12))
+            amount_owned_label.grid(row=i+1, column=2)
+    
+            approx_value_label = ctk.CTkLabel(self.watch_frame, text=f"${approx_value}", font=("Helvetica", 12))
+            approx_value_label.grid(row=i+1, column=3)
+        
+
+    def run(self):
+        self.window()
+        self.app.mainloop()
+
+
+
+
+app = TraderApp()
+app.run()
+
+
+
+
+
